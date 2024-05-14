e26f6d70902d7501fd213143ebf17b08a0814f01
Celso Delfino <50994973+CelsoDelfino@users.noreply.github.com>
2024-05-14 14:23:37 -0300

    add create e getById no usuarioController

--- src/main/java/com/github/parkapi/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/github/parkapi/demoparkapi/service/UsuarioService.java
@@ -1,7 +1,9 @@
 package com.github.parkapi.demoparkapi.service;
 
+import com.github.parkapi.demoparkapi.entity.Usuario;
 import com.github.parkapi.demoparkapi.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioService {
@@ -12,6 +14,17 @@ public class UsuarioService {
         this.usuarioRepository = usuarioRepository;
     }
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return this.usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado!")
+        );
+    }
 
 
 }
--- src/main/java/com/github/parkapi/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/github/parkapi/demoparkapi/web/controller/UsuarioController.java
@@ -1,8 +1,10 @@
 package com.github.parkapi.demoparkapi.web.controller;
 
+import com.github.parkapi.demoparkapi.entity.Usuario;
 import com.github.parkapi.demoparkapi.service.UsuarioService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -14,5 +16,15 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
-    
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        Usuario user = this.usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = this.usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
 }
